b6dff7ae7bc54623d7895d2678006f37b50bf027
NiklasRulec <niklasrulec.98@gmail.com>
2023-06-09 18:05:42 +0200

    react-beer-app

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/react-beer-app",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/react-beer-app/";
+  }
+
+  return config;
 });
